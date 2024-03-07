ee19e38ad887f8b431ead234260936da5a3df490
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-03-08 00:28:33 +0900

    CategoryControllerTest, AddressControllerTest, OrderControllerTest 완료

--- src/test/java/com/nhnacademy/shoppingmall/controller/AddressControllerTest.java
+++ src/test/java/com/nhnacademy/shoppingmall/controller/AddressControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -99,4 +98,5 @@ class AddressControllerTest {
         mockMvc.perform(delete("/api/address/{addressId}", 100))
                 .andExpect(status().isOk());
     }
+
 }
